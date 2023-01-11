3c7a804cef134ed1a10767dc4f5d05901cf4c701
Emmanuel Oscar <emmanuel001j@gmail.com>
2023-01-11 15:42:16 -0800

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""This script that adds all arguments
-to a Python list, and then save them to a file
-"""
-
-
-import sys
 from os import path
-"""Import the json module and sys module"""
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-"""Import the save to json file function"""
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""Import the save to json file function"""
 
-ret_list = []
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-if path.isfile("add_item.json") is False:
-    save_to_json_file(ret_list, "add_item.json")
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
 
-ret_list = load_from_json_file("add_item.json")
-
-if sys.argv[1:]:
-    for i in sys.argv[1:]:
-        ret_list.append(i)
-
-save_to_json_file(ret_list, "add_item.json")
+save_to_json_file(obj_json_file, 'add_item.json')
